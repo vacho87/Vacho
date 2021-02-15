5d6cd9329ffe433079d7df914fdf3ffdb3e93833
Vasily Kozhikhov <vacho.87@gmail.com>
2021-02-16 01:30:23 +0300

    Implemented script for generating DB. EF object model will be redesigned

---
+++ BTdbManagement/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace BTdbManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
