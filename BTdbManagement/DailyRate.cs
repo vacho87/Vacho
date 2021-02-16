67614824f1009758b4ff765fbf8c2c514b012903
Vasily Kozhikhov <vacho.87@gmail.com>
2021-02-16 13:32:05 +0300

    Revert "Revert "Implemented script for generating DB. EF object model will be redesigned""
    This reverts commit 9ee1148090c244f1e10e786446f9cff2bba265b3.

---
+++ BTdbManagement/DailyRate.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BTdbManagement
+{
+    public partial class DailyRate
+    {
+        public byte Id { get; set; }
+        public DateTime BeginDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public decimal Rate { get; set; }
+    }
+}
---
+++ BTdbManagement/OrderInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BTdbManagement
+{
+    public partial class OrderInfo
+    {
+        public short Id { get; set; }
+        public int? BusinessTripId { get; set; }
+        public short OrderNumber { get; set; }
+        public DateTime OrderDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public byte LocalityId { get; set; }
+        public bool Permanent { get; set; }
+        public bool TrafficForward { get; set; }
+        public bool TrafficBack { get; set; }
+        public bool Feeding { get; set; }
+        public bool Transport { get; set; }
+        public bool Lodging { get; set; }
+        public byte RationPack { get; set; }
+        public short TeamId { get; set; }
+        public byte PurposeId { get; set; }
+
+        public virtual BusinessTrip BusinessTrip { get; set; }
+        public virtual Locality Locality { get; set; }
+        public virtual BusinessTripPurpose Purpose { get; set; }
+    }
+}
