8a8b954ec737ff71f56faef821419bc1b3c47077
Vasily Kozhikhov <vacho.87@gmail.com>
2021-02-16 13:34:35 +0300

    Revert "Revert "Implemented script for generating DB. EF object model will be redesigned""
    This reverts commit 9ee1148090c244f1e10e786446f9cff2bba265b3.

---
+++ BTdbManagement/DwellingRate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace BTdbManagement
+{
+    public partial class DwellingRate
+    {
+        public byte Id { get; set; }
+        public byte? LocalityTypeId { get; set; }
+        public DateTime BeginDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public decimal Rate { get; set; }
+
+        public virtual LocalityType LocalityType { get; set; }
+    }
+}
